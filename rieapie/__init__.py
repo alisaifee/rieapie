4bbaf3a839b059b274cae6ef2a364a1e76c2544a
Ali-Akber Saifee <ali@indydevs.org>
2013-08-11 09:27:46 +0800

    temporary hack to resolve dependency resolution during setup

--- rieapie/__init__.py
+++ rieapie/__init__.py
@@ -1,5 +1,8 @@
 """
 """
-from rieapie.trickery import Api, pre_request,post_request
-import rieapie.wrappers
 version = "0.0.6.1"
+try:
+    from rieapie.trickery import Api, pre_request,post_request
+    import rieapie.wrappers
+except ImportError,e:
+    print("error initializing rieapie")
